24211e868141ac9aedc2562ff40d1873eec21103
Huỳnh Minh Phước <118465492+PhuocHuynh63@users.noreply.github.com>
2024-12-28 23:49:27 +0700

    UI history

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import SideBarMain from "@/containers/Sidebar";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "@/styles/layout.style.scss";
-import '@/app/globals.css';
 import HeaderHome from "@/components/HeaderHome/HeaderHome";
 import mgeService from "@/apiRequests/mge";
 import { IMGE } from "@/shemaValidations/model.schema";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "@/styles/layout.style.scss";
+import '@/app/globals.css';
 
 const RootLayout: React.FC<{ children: React.ReactNode }> = async ({ children }) => {
   const mgeResponse = await mgeService.getMge(1, 5) as IMGE;
--- src/containers/History/index.tsx
+++ src/containers/History/index.tsx
@@ -8,58 +8,79 @@ const HistoryPage = () => {
             <div className="container my-3 mb-5">
                 <div className="row mx-3 g-4">
                     <div className="col-lg-4 col-md-6 col-sm-12">
-                        <div className="card ">
+                        <div className="card">
                             <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <h5 className="card-title card_ingame">INGAME: GOL PHUOC</h5>
+                                <h5 className="card-title card_id">ID: 69128946</h5>
+                                <span className={`card-points negative`}>{(10000000).toLocaleString('vi-vn')}</span>
+                                <p className="card-text card-description">Bid MGE Mixtroops</p>
+                                <span className='card-time'>2024/12/28 - 01:18:55 (UTC)</span>
                             </div>
                         </div>
                     </div>
                     <div className="col-lg-4 col-md-6 col-sm-12">
-                        <div className="card ">
+                        <div className="card">
                             <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <h5 className="card-title card_ingame">INGAME: GOL PHUOC</h5>
+                                <h5 className="card-title card_id">ID: 69128946</h5>
+                                <span className={`card-points positive`}>{(10000000).toLocaleString('vi-vn')}</span>
+                                <p className="card-text card-description">Bid MGE Mixtroops</p>
+                                <span className='card-time'>2024/12/28 - 01:18:55 (UTC)</span>
                             </div>
                         </div>
                     </div>
                     <div className="col-lg-4 col-md-6 col-sm-12">
-                        <div className="card ">
+                        <div className="card">
                             <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <h5 className="card-title card_ingame">INGAME: GOL PHUOC</h5>
+                                <h5 className="card-title card_id">ID: 69128946</h5>
+                                <span className={`card-points positive`}>{(10000000).toLocaleString('vi-vn')}</span>
+                                <p className="card-text card-description">Bid MGE Mixtroops</p>
+                                <span className='card-time'>2024/12/28 - 01:18:55 (UTC)</span>
                             </div>
                         </div>
                     </div>
                     <div className="col-lg-4 col-md-6 col-sm-12">
-                        <div className="card ">
+                        <div className="card">
                             <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <h5 className="card-title card_ingame">INGAME: GOL PHUOC</h5>
+                                <h5 className="card-title card_id">ID: 69128946</h5>
+                                <span className={`card-points positive`}>{(10000000).toLocaleString('vi-vn')}</span>
+                                <p className="card-text card-description">Bid MGE Mixtroops</p>
+                                <span className='card-time'>2024/12/28 - 01:18:55 (UTC)</span>
                             </div>
                         </div>
                     </div>
                     <div className="col-lg-4 col-md-6 col-sm-12">
-                        <div className="card ">
+                        <div className="card">
                             <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <h5 className="card-title card_ingame">INGAME: GOL PHUOC</h5>
+                                <h5 className="card-title card_id">ID: 69128946</h5>
+                                <span className={`card-points positive`}>{(10000000).toLocaleString('vi-vn')}</span>
+                                <p className="card-text card-description">Bid MGE Mixtroops</p>
+                                <span className='card-time'>2024/12/28 - 01:18:55 (UTC)</span>
                             </div>
                         </div>
                     </div>
                     <div className="col-lg-4 col-md-6 col-sm-12">
-                        <div className="card ">
+                        <div className="card">
                             <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <h5 className="card-title card_ingame">INGAME: GOL PHUOC</h5>
+                                <h5 className="card-title card_id">ID: 69128946</h5>
+                                <span className={`card-points negative`}>{(10000000).toLocaleString('vi-vn')}</span>
+                                <p className="card-text card-description">Bid MGE Mixtroops</p>
+                                <span className='card-time'>2024/12/28 - 01:18:55 (UTC)</span>
                             </div>
                         </div>
                     </div>
                     <div className="col-lg-4 col-md-6 col-sm-12">
-                        <div className="card ">
+                        <div className="card">
                             <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <h5 className="card-title card_ingame">INGAME: GOL PHUOC</h5>
+                                <h5 className="card-title card_id">ID: 69128946</h5>
+                                <span className={`card-points negative`}>{(10000000).toLocaleString('vi-vn')}</span>
+                                <p className="card-text card-description">Bid MGE Mixtroops</p>
+                                <span className='card-time'>2024/12/28 - 01:18:55 (UTC)</span>
                             </div>
                         </div>
                     </div>
