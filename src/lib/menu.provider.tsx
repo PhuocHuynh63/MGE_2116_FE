4c6d3c9ad940fa77180801de9a7c1ba60df897aa
Huỳnh Minh Phước <118465492+PhuocHuynh63@users.noreply.github.com>
2025-01-03 13:11:45 +0700

    responsive sidebar for mobile provider

--- src/lib/menu.provider.tsx
+++ src/lib/menu.provider.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { createContext, useState, useContext, ReactNode } from 'react';
+import React, { createContext, useState, useContext, ReactNode, useEffect } from 'react';
 
 interface IMenuContext {
     isMenuOpen: boolean;
@@ -9,15 +9,30 @@ interface IMenuContext {
 const MenuContext = createContext<IMenuContext | undefined>(undefined);
 
 export const MenuProvider = ({ children }: { children: ReactNode }) => {
-    const [isMenuOpen, setIsMenuOpen] = useState(true);
+    const [isMenuOpen, setIsMenuOpen] = useState<boolean>(() => {
+        if (typeof window !== 'undefined') {
+            return window.innerWidth >= 769;
+        }
+        return true;
+    });
+
+    const [isInitialized, setIsInitialized] = useState(false);
+
+    useEffect(() => {
+        if (window.innerWidth < 769) {
+            setIsMenuOpen(false);
+        }
+        setIsInitialized(true);
+    }, []);
 
     const toggleMenu = () => {
         setIsMenuOpen((prev) => !prev);
     };
 
+
     return (
         <MenuContext.Provider value={{ isMenuOpen, toggleMenu }}>
-            {children}
+            {isInitialized && children}
         </MenuContext.Provider>
     );
 };
